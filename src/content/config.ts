dfabc44f2657c8de7ebef8404bcf3b7b4b5bbec5
pablocru <pablocru@outlook.com>
2024-06-14 14:37:44 +0200

    refactor: Use Astro Content Collection to manage the default and home page meta tags

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { z, defineCollection } from "astro:content";
+
+const pageSchema = z.object({
+  tabTitle: z.string(),
+  pageTitle: z.string(),
+  seoTitle: z.string().min(30).max(60),
+  seoDescription: z.string().min(60).max(155),
+});
+
+export const collections = {
+  homePage: defineCollection({
+    type: "data",
+    schema: z.object({
+      pageDescription: z.string(),
+    }).and(pageSchema)
+  })
+};
