8674ebdbc7d2bc4bb59e27c9d0952beef5d2923d
pablocru <pablocru@outlook.com>
2024-06-15 15:54:00 +0200

    feat: Merge all Content Collections for static pages to provide a common structure

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,14 @@
 import { z, defineCollection } from "astro:content";
 
 const pageSchema = z.object({
-  tabTitle: z.string(),
+  pageDescription: z.string(),
   pageTitle: z.string(),
-  seoTitle: z.string().min(30).max(60),
   seoDescription: z.string().min(60).max(155),
+  seoTitle: z.string().min(30).max(60),
+  tabTitle: z.string(),
 });
 
 export const collections = {
-  homePage: defineCollection({
-    type: "data",
-    schema: z.object({
-      pageDescription: z.string(),
-    }).and(pageSchema)
-  }),
   staticPage: defineCollection({
     type: "data",
     schema: pageSchema
